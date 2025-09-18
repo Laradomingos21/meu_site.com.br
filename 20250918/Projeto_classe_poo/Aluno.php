c9261b7f0865d83368d61d1f635b76a84b1d439a
Laradomingos21 <lara.v.domingos@aluno.senai.br>
2025-09-18 15:54:13 -0300

    PHP

---
+++ 20250918/Projeto_classe_poo/Aluno.php
@@ -0,0 +1,26 @@
+<?php
+require_once "Usuario.php";
+
+//Class Filha - Aluno
+Class Aluno extends Usuario {
+    private $matricula;
+
+    public function __construct($nome, $email, $matricula) {
+        parent::__construct($nome, $email);
+        $this->matricula = $matricula;
+    }
+
+    public function getMatricula() {
+        return $this->matricula;
+    }
+
+    public function exibirInfo() {
+        return parent::exibirInfo() . " | Matrícula: {$this->matricula}";
+    }
+
+    public function estudar() {
+        return "{$this->nome} está estudando...";
+    }
+}
+
+?>
