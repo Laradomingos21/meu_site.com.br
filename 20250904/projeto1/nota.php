49ca5a9ac98797ba7dda32cfddf3983f212d2c2d
Laradomingos21 <lara.v.domingos@aluno.senai.br>
2025-09-25 08:01:32 -0300

    PHP

---
+++ 20250904/projeto1/nota.php
@@ -0,0 +1,12 @@
+<h1>Calculadora de média</h1>
+<form action="calcula_nota.php" method="post">
+    <b>Nome do aluno: </b><br>
+    <input type="text" name="nome" required><br>
+    <b>Quantas notas são? <br></b>
+    <input type="text" name="nnotas" required><br>
+    <b>Qual é a soma das notas? <br></b>
+    <input type="text" name="snotas" required><br>
+    <b>Qual é a disciplina? <br></b>
+    <input type="text" name="materia" required><br><br>
+    <input type="submit" value="entrar"><br>
+</form>
